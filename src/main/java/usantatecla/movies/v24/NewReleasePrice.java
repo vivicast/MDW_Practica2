afa27094f8eb852638b16b8df645fa001c939444
Viviana Castillo <v.castillo@alumnos.upm.es>
2021-04-23 00:27:47 +0200

    V24: NewReleasePrice hereda de Movie

--- src/main/java/usantatecla/movies/v24/NewReleasePrice.java
+++ src/main/java/usantatecla/movies/v24/NewReleasePrice.java
@@ -1,11 +1,15 @@
 package usantatecla.movies.v24;
 
-public class NewReleasePrice extends Price {
+public class NewReleasePrice extends Movie {
 
 	private static final double CHARGE = 3;
 	private static final int DAYS_RENTED_THRESHOLD = 1;
 	private static final int FREQUENT_RENTER_POINTS = 2;
-	
+
+	public NewReleasePrice(String title) {
+		super(title);
+	}
+
 	@Override
 	public double getCharge(int daysRented) {
 		return NewReleasePrice.CHARGE;
